cb88208e52c2dc44dd62783e8c7895d544c7ed6b
supreme886 <985636263@qq.com>
2025-07-11 17:38:31 +0800

    add log module

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,65 @@
 #include <QTimer>
 #include <QPluginLoader>
 #include <QMessageBox>
+#include <QFile>
+#include <QTextCodec>
+#include <QTextStream>
+#include <QDateTime>
+#include <QMutex>
 
 #include "mainwindow.h"
-#include <QFile>
+
+void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+    static QMutex mutex;
+    mutex.lock();
+
+    QString time = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz");
+    QString logLevel;
+
+    switch (type) {
+    case QtDebugMsg:     logLevel = "[DEBUG]"; break;
+    case QtInfoMsg:      logLevel = "[INFO]";  break;
+    case QtWarningMsg:   logLevel = "[WARN]";  break;
+    case QtCriticalMsg:  logLevel = "[ERROR]"; break;
+    case QtFatalMsg:     logLevel = "[FATAL]"; break;
+    default:             logLevel = "[UNKNOWN]";
+    }
+
+    QString logMessage = QString("%1 %2 %3 (%4:%5): %6\n")
+                             .arg(time)
+                             .arg(logLevel)
+                             .arg(context.category)
+                             .arg(context.file)
+                             .arg(context.line)
+                             .arg(msg);
+
+    QString logDir = QCoreApplication::applicationDirPath() + "/logs";
+    QDir().mkpath(logDir);
+
+    QFile file(logDir + "/app.log");
+    if (file.open(QIODevice::Append | QIODevice::Text)) {
+        QTextStream out(&file);
+        out << logMessage;
+        file.close();
+    }
+
+    QTextStream console(stdout);
+    // 设置控制台输出编码为GBK以解决中文乱码问题
+#ifdef Q_OS_WIN
+    console.setCodec("GBK");
+#else
+    // 非Windows系统使用UTF-8
+    console.setCodec("UTF-8");
+#endif
+    console << logMessage;
+
+    mutex.unlock();
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(customMessageHandler);
+
     QApplication app(argc, argv);
     
     // 设置应用程序信息
